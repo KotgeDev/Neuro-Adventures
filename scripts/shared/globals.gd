3fec574be694968da01f7061733abf44ce83783b
KotgeDev <68812485+KotgeDev@users.noreply.github.com>
2023-12-25 02:25:55 +0900

    restructured upgrades

--- scripts/shared/globals.gd
+++ scripts/shared/globals.gd
@@ -1,5 +1,8 @@
 extends Node
 
+var current_collab_partner = WhichCollabPartner.VEDAL	
+var current_ai = WhichAI.NEURO
+
 #region Enums 
 enum UpgradeType {
 	AI_UPGRADE,
@@ -10,6 +13,14 @@ enum Owners {
 	OWNED_BY_AI,
 	OWNED_BY_COLLAB_PARTNER,
 	OWNED_BY_ENEMY
+}
+
+enum WhichCollabPartner {
+	VEDAL
+}
+
+enum WhichAI {
+	NEURO 
 }
 #endregion 
 
@@ -31,7 +42,6 @@ signal send_random_upgrades(upgrades: Array)
 #endregion 
 
 #region TO UPGRADE MANAGER
-signal add_specific_upgrades(upgrades: Array) 
 signal get_random_upgrades
 signal lvl_up(upgrade: Upgrade)
 #endregion 
---
+++ scripts/upgrades/gaslight.gd
@@ -0,0 +1,27 @@
+extends UpgradeScene
+
+func _ready() -> void: 
+	add_to_group("process_ai_damage_received")
+
+func process_ai_damage_received(BASE_DAMAGE: float, modified_damage: float) -> float:
+	match upgrade.lvl: 
+		1:
+			modified_damage = get_gaslight_damage(modified_damage, 5, 10)
+		2:
+			modified_damage = get_gaslight_damage(modified_damage, 7, 15) 
+		3:
+			modified_damage = get_gaslight_damage(modified_damage, 10, 20)
+		4:   
+			modified_damage = get_gaslight_damage(modified_damage, 10, 20)
+
+	return modified_damage 
+
+func get_gaslight_damage(damage: float, ignore_chance: int, divert_chance:int) -> float: 
+	var random_int = randi_range(1, 100)
+	
+	if random_int <= ignore_chance: 
+		damage = 0.0
+	elif ignore_chance < random_int and random_int <= (ignore_chance + divert_chance):
+		Globals.damage_collab_partner.emit(damage)
+		damage = 0.0 
+	return damage 
