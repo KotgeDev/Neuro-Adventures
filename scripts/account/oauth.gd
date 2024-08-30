06500630b6556c926afab69150cb2f5c56cf14cb
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2024-08-30 16:28:51 +0200

    Use better oauth implementation and fix web version.

--- scripts/account/oauth.gd
+++ scripts/account/oauth.gd
@@ -7,17 +7,17 @@ var PORT: int
 var BINDING: String
 var CLIENT_ID: String
 var REDIRECT_URI: String
-var AUTH_SERVER := "https://discord.com/oauth2/authorize" 
+var AUTH_SERVER := "https://discord.com/oauth2/authorize"
 var API_ENDPOINT := "https://discord.com/api/v10"
 
-var redirect_server := TCPServer.new() 
+var state = null
 
-var token: String 
-var refresh_token: String 
+var token: String
+var refresh_token: String
 
 # Server 
-var SERVER_ADDRESS: String 
-var SERVER_PASSWORD: String 
+var SERVER_ADDRESS: String
+var SERVER_PASSWORD: String
 
 func setup(config: ConfigFile) -> void:
 	set_process(false)
@@ -28,70 +28,69 @@ func setup(config: ConfigFile) -> void:
 	REDIRECT_URI = config.get_value("oauth", "redirect_uri")
 
 	SERVER_ADDRESS = config.get_value("server", "address")
-	SERVER_PASSWORD = config.get_value("server", "password") 
+	SERVER_PASSWORD = config.get_value("server", "password")
 	
-	load_tokens() 
+	load_tokens()
 
 #func validate_tokens() -> void:
 	#if !(await is_token_valid()):
 		#if !(await refresh_tokens()):
 			#get_auth_code()
 
-func _process(delta: float) -> void:
-	if redirect_server.is_connection_available():
-		var connection = redirect_server.take_connection()
-		var request = connection.get_string(connection.get_available_bytes())
-		if request:
-			set_process(false)
-			var auth_code = request.split(" HTTP")[0].split("=")[1]
-			_get_token_from_auth(auth_code)
-			
-			connection.put_data(("HTTP/1.1 %d\r\n" % 200).to_ascii_buffer())
-			connection.put_data(load_HTML("res://scripts/account/display_page.html").to_ascii_buffer())
-			redirect_server.stop() 
+func generate_secure_token(length: int) -> String:
+	var crypto = Crypto.new()
+	return crypto.generate_random_bytes(length / 2).hex_encode()
 
 func get_auth_code() -> void:
-	set_process(true)
-	
-	var redir_err = redirect_server.listen(PORT, BINDING)
-	
+	state = generate_secure_token(16)
+
 	var data = [
 		"response_type=code",
 		"client_id=%s" % CLIENT_ID,
 		"scope=identify",
 		"redirect_uri=%s" % REDIRECT_URI,
+		"state=%s" % state
 	]
 	var body = "&".join(data)
 	
 	var url = AUTH_SERVER + "?" + body
 	
 	OS.shell_open(url)
+	_get_token_from_auth()
 
-func _get_token_from_auth(auth_code):
-	print("Got auth code! Sending to server for accesss tokens ...  ")
-	
+func _token_get_failed() -> void:
+	# TODO: Inform the user
+	print("Token request timed out")
+
+func _get_token_from_auth():
 	var headers = ["Content-Type: application/json"]
 	
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
 	
 	var data = {
-		"auth_code": auth_code
+		"state": state
 	}
 	
-	var body = RequestBody.serialize("get_access_token", data)
+	var body = RequestBody.serialize("get_access_token_from_state", data)
 	
-	http_request.request(SERVER_ADDRESS, headers, HTTPClient.METHOD_POST, body)
-	
-	var response = await http_request.request_completed
-	var response_body = JSON.parse_string(response[3].get_string_from_utf8()) 
-	
+	get_tree().create_timer(3 * 60).timeout.connect(_token_get_failed)
+
+	var response = [0, 0]
+	while response[1] != 200:
+		await get_tree().create_timer(0.3).timeout
+		print("Requesting token from server ...")
+		http_request.request(SERVER_ADDRESS, headers, HTTPClient.METHOD_POST, body)
+		response = await http_request.request_completed
+
+	var response_body = JSON.parse_string(response[3].get_string_from_utf8())
+		
 	token = response_body["access_token"]
 	refresh_token = response_body["refresh_token"]
-	
-	save_tokens() 
+		
+	save_tokens()
 	token_recieved.emit()
-	
+		
 	http_request.queue_free()
 	print("Tokens received!")
 
@@ -108,7 +107,7 @@ func get_user_data() -> Dictionary:
 
 	var response = await http_request.request_completed
 	
-	var response_body = JSON.parse_string(response[3].get_string_from_utf8()) 
+	var response_body = JSON.parse_string(response[3].get_string_from_utf8())
 	
 	return response_body
 
@@ -163,7 +162,7 @@ func revoke_tokens() -> void:
 	http_request.request(SERVER_ADDRESS, headers, HTTPClient.METHOD_POST, body)
 	
 	var response = await http_request.request_completed
-	var response_body = JSON.parse_string(response[3].get_string_from_utf8()) 
+	var response_body = JSON.parse_string(response[3].get_string_from_utf8())
 	
 	if response[1] == 200:
 		print("Tokens revoked!")
@@ -186,10 +185,10 @@ func save_tokens():
 func load_tokens():
 	if FileAccess.file_exists(SAVE_DIR):
 		var file = FileAccess.open_encrypted_with_pass(SAVE_DIR, FileAccess.READ, "727")
-		var tokens = file.get_var() 
+		var tokens = file.get_var()
 		token = tokens["token"]
 		refresh_token = tokens["refresh_token"]
-		file.close() 
+		file.close()
 	else:
 		token = ""
 		refresh_token = ""
@@ -197,7 +196,7 @@ func load_tokens():
 func load_HTML(path):
 	var file = FileAccess.open(path, FileAccess.READ)
 	var HTML = file.get_as_text().replace("    ", "\t").insert(0, "\n")
-	file.close() 
+	file.close()
 	
-	return HTML 
+	return HTML
 #endregion 
